bf7ede6343e123203555d440a0dd5ac00bcac3c3
ChristianNonti <93360281+ChristianNonti@users.noreply.github.com>
2022-12-08 16:10:08 +0700

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+Susu = 20000
+Permen = 500
+roti = 15000
+indomie = 3000
+vitamin = 50000
+
+inputan = input("Masukkan jumlah uang :")
+start = input("Ketik 'start' untuk memulai :")
+barang = input("barang apa yang ingin anda beli? :")
+
